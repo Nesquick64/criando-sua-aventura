ccb22d499143db7908b386167b0fb30fa7f9be8c
Nesquick64 <0000110599014xsp@al.educacao.sp.gov.br>
2024-08-29 16:31:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){const avanca = document.querySelectorAll('.btn-proximo');
+
+        avanca.forEach(button => {
+            button.addEventListener('click', function(){
+                const atual - document.querySelector('.ativo');
+             })
+        })
+    
+     })
+})const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual - document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute()
+     })
+})const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual - document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+     })
+})const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})<!-- código omitido -->
+
+<script src="script.js"></script>
+</body>
+</html>
